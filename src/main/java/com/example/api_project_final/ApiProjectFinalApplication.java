5ec2e924dcf36961d99a7a3055335c895812195a
Shiv991319 <shivanshu.mishra@olx.in>
2025-04-10 19:38:01 +0530

    Initiate

---
+++ src/main/java/com/example/api_project_final/ApiProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_project_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProjectFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_project_final/ApiProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_project_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
